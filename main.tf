a27c8c837ecbb7897bd156fcd447b65365fcb91f
Smarana15 <smaranam1593@gmail.com>
2025-07-03 22:46:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,20 @@
 provider "aws" {
   region = "ap-south-1"
 }
+resource "aws_dynamodb_table" "state_locking" {
+  hash_key = "LockID"
+  name     = "dynamodb-state-locking"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  billing_mode = "PAY_PER_REQUEST"
+}
+terraform {
+    backend "s3" {
+        bucket = "smaranas3bucket"
+        key    = "statefilesmarus3/terraform.tfstate"
+        region     = "ap-south-1"
+       dynamodb_table  = "dynamodb-state-locking"
+    }
+} 
