25efb56f3182c5ae3da0c4b9035ed54725c74708
Smarana15 <smaranam1593@gmail.com>
2025-07-03 22:37:09 +0530

    Update asg.tf

--- asg.tf
+++ asg.tf
@@ -1,31 +1,48 @@
-
-resource "aws_launch_configuration" "as_conf" {
-  name_prefix   = "terraform-lc-example-"
+resource "aws_launch_template" "asg_template" {
+  name_prefix   = "asg_lt_example"
   image_id      = "${var.ami}"
   instance_type = "${var.instance_type}"
-  key_name = "${var.terraform-key-pair}"
-  security_groups = ["${aws_security_group.dev-sg.id}"]
-  associate_public_ip_address = true
+  vpc_security_group_ids = [aws_security_group.dev-sg.id]
+   lifecycle {
+     create_before_destroy = true
+   }
 
-  lifecycle {
-    create_before_destroy = true
+   tags = {
+    Name = "asg-lt_example"
   }
 }
 
-
-
-resource "aws_autoscaling_group" "autoscaling_group_dev" {
-  launch_configuration = "${aws_launch_configuration.as_conf.id}"
-  min_size             = "${var.autoscaling_group_min_size}"
-  max_size             = "${var.autoscaling_group_max_size}"
-  target_group_arns    = ["${aws_lb_target_group.front-end.arn}"]
-vpc_zone_identifier  = [
-    aws_subnet.public.id,
-    aws_subnet.private.id
-  ]
-  tag {
-    key                 = "Name"
-    value               = "autoscaling-group-dev"
-    propagate_at_launch = true
+resource "aws_autoscaling_group" "asg" {
+  name                      = "asg"
+  min_size                  = "${var.autoscaling_group_min_size}"
+  max_size                  = "${var.autoscaling_group_max_size}"
+  desired_capacity          = 2
+  health_check_grace_period = 5
+  health_check_type         = "ELB"
+  vpc_zone_identifier       = [aws_subnet.public.id,aws_subnet.private.id]
+  launch_template {
+    id      = aws_launch_template.asg_template.id
+    version = "$Latest"
   }
+
 }
+
+resource "aws_autoscaling_policy" "avg_cpu_policy_greater" {
+  name                   = "avg-cpu-policy-greater"
+  policy_type            = "TargetTrackingScaling"
+  autoscaling_group_name = aws_autoscaling_group.asg.id
+  # CPU Utilization is above 50
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 50.0
+  }
+
+}
+
+resource "aws_autoscaling_attachment" "asg_attachment" {
+  autoscaling_group_name = aws_autoscaling_group.asg.id
+  lb_target_group_arn    = aws_lb_target_group.front-end.arn
+}
+~
